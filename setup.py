4d473e0445138ca956a0d2546e9b34848ec7a9f8
himaj47 <himajjoshi932@gmail.com>
2025-02-01 17:08:44 +0530

    added license and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_module",
+    version="0.1.0",
+    author="himaj joshi",
+    author_email="himajjoshi932@gmail.com",
+    description="A Python package for ROS2 integration with FROS user interface",
+    url="https://github.com/himaj47/Qt-FROS-custom-widget",
+    packages=find_packages(),
+    install_requires=[
+        "PySide2>=5.15.2",
+        "setuptools",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+        ],
+    },
+)

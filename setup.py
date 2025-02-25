b16efe7602941c234a972da0593cd5d20e319acb
himaj47 <himajjoshi932@gmail.com>
2025-02-25 12:53:04 +0530

    updated module name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_module",
+    name="Custom_Widgets",
     version="0.1.0",
     author="himaj joshi",
     author_email="himajjoshi932@gmail.com",
